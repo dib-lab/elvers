62c8bcc24d6562a548141013897e28a188953c4f
Tessa Pierce <ntpierce@gmail.com>
2018-06-20 12:13:44 -0400

    full khmer rule

---
+++ rules/khmer/khmer_targets.py
@@ -0,0 +1,15 @@
+from os import path
+from common.utils import is_se
+
+def get_targets(units, basename, outdir, extensions = ['.gz'], se_ext = ['.single'], pe_ext = ['.paired.1','.paired.2','.single']):
+    """
+    Use the sample info provided in the tsv file
+    to generate required targets for trimmomatic
+    """
+    trim_targs = []
+    for s, u in units.iterrows():
+        sample, unit = u['sample'],u['unit']
+        end = se_ext if is_se(units,sample,unit) else pe_ext
+        trim_targs = trim_targs +  ['{}_{}_'.format(sample,unit) + i + j for i in end for j in extensions]
+
+    return [path.join(outdir, targ) for targ in trim_targs]
