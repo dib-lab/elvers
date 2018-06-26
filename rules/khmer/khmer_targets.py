cbcba7b20f10aa48377f1bf3991b0f0c71b4c728
Tessa Pierce <ntpierce@gmail.com>
2018-06-26 18:04:45 -0400

    mostly working khmer pe

--- rules/khmer/khmer_targets.py
+++ rules/khmer/khmer_targets.py
@@ -1,7 +1,7 @@
 from os import path
 from common.utils import is_se
 
-def get_targets(units, basename, outdir, extensions = ['.gz'], se_ext = ['.single'], pe_ext = ['.paired', '.single', '.paired.1','.paired.2']):
+def get_targets(units, basename, outdir, extensions = ['.gz'], se_ext = ['.se'], pe_ext = ['.paired', '.single', '.paired.1','.paired.2']):
     """
     Use the sample info provided in the tsv file
     to generate required targets for trimmomatic
@@ -11,5 +11,4 @@ def get_targets(units, basename, outdir, extensions = ['.gz'], se_ext = ['.singl
         sample, unit = u['sample'],u['unit']
         end = se_ext if is_se(units,sample,unit) else pe_ext
         trim_targs = trim_targs +  ['{}_{}_'.format(sample,unit) + i + j for i in end for j in extensions]
-
     return [path.join(outdir, targ) for targ in trim_targs]
