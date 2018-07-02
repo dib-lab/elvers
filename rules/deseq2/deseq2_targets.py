652b9bc8199c27d003dfd95060e92decb20efc99
Tessa Pierce <ntpierce@gmail.com>
2018-07-02 21:58:16 +0000

    init revise deseq2 targs

--- rules/deseq2/deseq2_targets.py
+++ rules/deseq2/deseq2_targets.py
@@ -1,16 +1,23 @@
 from os import path
 from common.utils import is_se
 
-def get_targets(units, basename, outdir, extensions = ['.trim.fq.gz'], se_ext = ['se'], pe_ext = ['1','2']):
+def get_targets(samples, basename, outdir, extensions = ['.diffexp.tsv', '.ma-plot.svg', ], se_ext = ['se'], pe_ext = ['1','2']):
     """
     Use the sample info provided in the tsv file
     to generate required targets for trimmomatic
     """
-    trim_targs = []
-    for s, u in units.iterrows():
-        sample, unit = u['sample'],u['unit']
-        end = se_ext if is_se(units,sample,unit) else pe_ext
-        trim_targs = trim_targs +  ['{}_{}_'.format(sample,unit) + i + j for i in end for j in extensions]
+    de_targs = []
+    for sample, contrast in samples.iterrows():
+        sample, contrast = u['sample'],u['contrast']
+        de_targs = de_targs +  ['{}'.format(contrast) + i for i in extensions]
+    de_targs = de_targs + ['pca.svg']
+    return [path.join(outdir, targ) for targ in de_targs]
 
-    return [path.join(outdir, targ) for targ in trim_targs]
+# targs from rna-seq-star
+# expand(["results/diffexp/{contrast}.diffexp.tsv",
+#                "results/diffexp/{contrast}.ma-plot.svg"],
+#               contrast=config["diffexp"]["contrasts"]),
+#        "results/pca.svg"
+
+
 
