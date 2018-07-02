528e4098c50381ce138469df4a7286f972d44b32
Tessa Pierce <ntpierce@gmail.com>
2018-07-02 22:45:54 +0000

    more deseq2 init work

--- rules/deseq2/deseq2_targets.py
+++ rules/deseq2/deseq2_targets.py
@@ -2,23 +2,23 @@ from os.path import join
 from common.utils import get_params
 
 
-def get_targets(samples, basename, outdir, extensions = ['.diffexp.tsv', '.ma-plot.svg', ], se_ext = ['se'], pe_ext = ['1','2']):
+def get_targets(units, basename, outdir, extensions = ['.diffexp.tsv', '.ma-plot.svg', ], se_ext = ['se'], pe_ext = ['1','2']):
     """
-    Use the sample info provided in the tsv file
-    to generate required targets for trimmomatic
+    generate contrasts from deseq2 configfile #not best soln - think more.
     """
     # default contrast info from deseq2 yaml 
     contrast_info = get_params('deseq2')
     # override defaults with snakemake config (main configfile or snakemake --config)
-    if 'deseq2' in config.keys():
-        contrast_info.update(config['deseq2'])
-    contrast_list = contrast['contrasts']
+    # not gonna work - haven't gotten config yet in main snakefile?
+    #if 'deseq2' in config.keys():
+    #    contrast_info.update(config['deseq2'])
+    contrast_list = contrast_info['contrasts']
     # build targets
     de_targs = []
     for c in contrast_list: 
         de_targs = de_targs +  ['{}'.format(c) + i for i in extensions]
     de_targs = de_targs + ['pca.svg']
-    return [path.join(outdir, targ) for targ in de_targs]
+    return [join(outdir, targ) for targ in de_targs]
 
 # targs from rna-seq-star
 # expand(["results/diffexp/{contrast}.diffexp.tsv",
