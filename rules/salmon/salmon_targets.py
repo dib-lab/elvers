0725a97695b3d3fe784d69db60b1aefd81311304
Tessa Pierce <ntpierce@gmail.com>
2018-07-09 11:50:54 -0400

    working on fix for salmon - combine units

--- rules/salmon/salmon_targets.py
+++ rules/salmon/salmon_targets.py
@@ -1,4 +1,5 @@
 from os import path
+from common.utils import is_se
 
 def get_targets(units, assembly_basename, outdir, extensions = ['/quant.sf', '/lib_format_counts.json'], se_ext = ['se'], pe_ext = ['pe']):
     """
@@ -6,14 +7,19 @@ def get_targets(units, assembly_basename, outdir, extensions = ['/quant.sf', '/l
     to generate required targets for salmon
     """
     salmon_targs = []
-    pe_only = units[units['fq2'].notnull()]
-    for s, u in pe_only.iterrows():
-        sample= u['sample']
-        salmon_targs = salmon_targs + ['{}_{}_x_{}'.format(sample, pe_ext[0], assembly_basename) + i for i in extensions]
-    se_only = units[units['fq2'].isnull()]
-    for s, u in se_only.iterrows():
-        sample= u['sample']
-        salmon_targs = salmon_targs + ['{}_{}_x_{}'.format(sample, se_ext[0], assembly_basename) + i for i in extensions]
-    
+
+    # here we need to get all units belonging to a single sample.
+    # When I used 'groupby' on our initial test data, groups were
+    # A/AB, B/AB, rather than A, B, AB. I'm sure there's a way to
+    # do exact matching in groupby, but here's a hackaround for now.
+    samples = list(set(units['sample'].tolist()))
+    #by_sample = units.groupby(['sample'], sort = False)
+    for s in samples:
+        unit_list = units.groupby(level=0).get_group(s)['unit'].tolist()
+        for unit in unit_list:
+            if is_se(units,s,unit):
+                salmon_targs = salmon_targs + ['{}_{}_x_{}'.format(s, se_ext[0], assembly_basename) + i for i in extensions]
+            else:
+                salmon_targs = salmon_targs + ['{}_{}_x_{}'.format(s, pe_ext[0], assembly_basename) + i for i in extensions]
     salmon_targs = list(set(salmon_targs)) # elim any redundant targs
     return [path.join(outdir, targ) for targ in salmon_targs]
