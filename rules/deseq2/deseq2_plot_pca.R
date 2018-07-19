055b6dc09e8b4e7399265b489c41829199901888
Tessa Pierce <ntpierce@gmail.com>
2018-07-19 23:40:59 +0000

    re add plot pca

---
+++ rules/deseq2/deseq2_plot_pca.R
@@ -0,0 +1,16 @@
+#from https://github.com/snakemake-workflows/rna-seq-star-deseq2
+log <- file(snakemake@log[[1]], open="wt")
+sink(log)
+sink(log, type="message")
+
+library("DESeq2")
+
+# load deseq2 data
+dds <- readRDS(snakemake@input[[1]])
+
+# obtain normalized counts
+counts <- rlog(dds, blind=FALSE)
+#svg(snakemake@output[[1]])
+pdf(snakemake@output[[1]])
+plotPCA(counts, intgroup=snakemake@params[["pca_labels"]])
+dev.off()
