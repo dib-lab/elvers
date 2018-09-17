792f4568b75800b2087126e2a09da1fccb49daa1
Tessa Pierce <ntpierce@gmail.com>
2018-09-16 18:55:36 -0700

    upd trinity to latest version

--- rules/trinity/wrapper.py
+++ rules/trinity/wrapper.py
@@ -42,4 +42,4 @@ assert 'trinity' in outdir, "output directory name must contain 'trinity'"
 
 log = snakemake.log_fmt_shell(stdout=False, stderr=True)
 
-shell("Trinity {input_cmd} --CPU {snakemake.threads} --max_memory {max_memory} -seqType {seqtype} --output {outdir} {snakemake.params.extra} {log}")
+shell("Trinity {input_cmd} --CPU {snakemake.threads} --max_memory {max_memory} --seqType {seqtype} --output {outdir} {snakemake.params.extra} {log}")
