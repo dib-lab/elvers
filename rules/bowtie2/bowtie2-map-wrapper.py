a51613d298caf29adbaa7901eaf804d21d57f1e5
Tessa Pierce <ntpierce@gmail.com>
2019-01-18 15:04:13 -0800

    cleanup dirs; add some useful snakemake args

---
+++ rules/bowtie2/bowtie2-map-wrapper.py
@@ -0,0 +1,23 @@
+__author__ = "Johannes Köster"
+__copyright__ = "Copyright 2016, Johannes Köster"
+__email__ = "koester@jimmy.harvard.edu"
+__license__ = "MIT"
+
+
+from snakemake.shell import shell
+
+extra = snakemake.params.get("extra", "")
+log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+
+n = len(snakemake.input.sample)
+assert n == 1 or n == 2, "input->sample must have 1 (single-end) or 2 (paired-end) elements."
+
+if n == 1:
+    reads = "-U {}".format(*snakemake.input.sample)
+else:
+    reads = "-1 {} -2 {}".format(*snakemake.input.sample)
+
+shell(
+    "(bowtie2 --threads {snakemake.threads} {snakemake.params.extra} "
+    "-x {snakemake.params.index} {reads} "
+    "| samtools view -Sbh -o {snakemake.output[0]} -) {log}")
