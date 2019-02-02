7579ad5d9cd5c992af28eea9e9b4e8679747f2a8
Chaz Reid <charlesreid1@gmail.com>
2019-02-02 10:46:44 -0800

    Add eelpond tests/ directory (#75)
    * Add eelpond tests/ directory
    
    This adds individual unit tests for
    each of eelpond's flags.
    
    Not all of these flag unit tests are
    implemented yet, but the --dag and
    --dagfile flags are tested, as are
    the --help and --dry-run flags,
    to demonstrate how to do it.
    
    * fill in --dagpng test. clean up test_flags test
    
    * add pytest to environment.yml, add pytest command to travis

---
+++ tests/const.py
@@ -0,0 +1,8 @@
+import os
+
+here = os.path.dirname(os.path.abspath(__file__))
+
+run_eelpond_cmd = os.path.join(here,'..','run_eelpond')
+
+test_config_yaml = os.path.join(here,'test_config.yaml')
+
