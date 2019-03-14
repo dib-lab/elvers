7e60c64711f5cff35139b1484028e1049b9c5858
Tessa Pierce <ntpierce@gmail.com>
2019-03-13 18:24:57 -0700

    change run_elvers_cmd

--- tests/const.py
+++ tests/const.py
@@ -2,7 +2,7 @@ import os
 
 here = os.path.dirname(os.path.abspath(__file__))
 
-elvers_cmd = os.path.join(here,'..','elvers')
+elvers_cmd = 'elvers'
 
 test_config_yaml = os.path.join(here,'test_config.yaml')
 
