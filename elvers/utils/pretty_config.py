e5bdfabab1497919fba5633f17fda2f71cefd564
Tessa Pierce <ntpierce@gmail.com>
2019-03-15 17:37:10 -0700

    import read_yaml instead of reproducing code

--- elvers/utils/pretty_config.py
+++ elvers/utils/pretty_config.py
@@ -7,13 +7,7 @@ import argparse
 import sys
 import yaml
 
-def read_yaml(filename):
-    with open(filename, 'r') as stream:
-        try:
-            yamlD = yaml.load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-    return yamlD
+from .utils import read_yaml
 
 def pretty_name(targ):
     print_name = '\n#\n# '+ targ + '\n#\n\n'
@@ -27,9 +21,7 @@ def write_config(paramsD, targets, out = None):
         generalP = {}
         generalP['basename'] = paramsD.get('basename', 'elvers')
         generalP['experiment'] = paramsD.get('experiment', '_experiment1')
-        #generalP['samples'] = paramsD.get('samples', 'samples.tsv')
-
-        outConfig.write((pretty_name("Elvers Pipeline Configfile")))
+        outConfig.write((pretty_name("elvers pipeline configuration")))
         yaml.dump(generalP, stream=outConfig, indent=2,  default_flow_style=False)
     else:
         sys.stdout.write('\n\n')
