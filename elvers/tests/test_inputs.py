9fa17c760d1c0d7300c0f772da502236531d974e
Tessa Pierce <ntpierce@gmail.com>
2019-03-21 22:45:52 -0700

    upd test descriptions

--- elvers/tests/test_inputs.py
+++ elvers/tests/test_inputs.py
@@ -7,19 +7,17 @@ from .utils import capture_stdouterr
 
 class TestInputs(TestCase):
     """
-    This class runs tests of elvers config utilities:
-    --build_config
-    --extra-config
+    This class runs tests with improperly formatted inputs
     """
     def test_bad_samples_entry(self):
-        """Test the --build_config flag"""
+        """Test a configfile with no samples tsv specified"""
         configfile = os.path.realpath('test_files/bad-samples-entry.yaml')
         command = [elvers_cmd, configfile, '-n']
         p_out, p_err = capture_stdouterr(command,here)
         assert "Error: trying to run `get_data` workflow, but the samples tsv file is not specified in your configfile. Please fix." in p_err
 
     def test_bad_samples_tsv(self):
-        """Test the --build_config flag"""
+        """Test an improperly formatted samples tsv file (spaces instead of tabs)"""
         configfile = os.path.realpath('test_files/bad-samples-tsv.yaml')
         samples_tsv = os.path.realpath('test_files/bad-samples-tsv.tsv')
         command = [elvers_cmd, configfile, '-n']
