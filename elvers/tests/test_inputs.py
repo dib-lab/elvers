f6c70a5c9d64ca7dc99a06dbdbbc51de1c59896d
Tessa Pierce <ntpierce@gmail.com>
2019-03-22 11:33:17 -0700

    full paths for testing bad input files

--- elvers/tests/test_inputs.py
+++ elvers/tests/test_inputs.py
@@ -11,15 +11,15 @@ class TestInputs(TestCase):
     """
     def test_bad_samples_entry(self):
         """Test a configfile with no samples tsv specified"""
-        configfile = os.path.realpath('test_files/bad-samples-entry.yaml')
+        configfile = os.path.realpath(os.path.join(here,'test_files/bad-samples-entry.yaml'))
         command = [elvers_cmd, configfile, '-n']
         p_out, p_err = capture_stdouterr(command,here)
         assert "Error: trying to run `get_data` workflow, but the samples tsv file is not specified in your configfile. Please fix." in p_err
 
     def test_bad_samples_tsv(self):
         """Test an improperly formatted samples tsv file (spaces instead of tabs)"""
-        configfile = os.path.realpath('test_files/bad-samples-tsv.yaml')
-        samples_tsv = os.path.realpath('test_files/bad-samples-tsv.tsv')
+        configfile = os.path.realpath(os.path.join(here,'test_files/bad-samples-tsv.yaml'))
+        samples_tsv = os.path.realpath(os.path.join(here,'test_files/bad-samples-tsv.tsv'))
         command = [elvers_cmd, configfile, '-n']
         p_out, p_err = capture_stdouterr(command,here)
         assert f"{samples_tsv} file is not properly formatted. Please fix." in p_err
