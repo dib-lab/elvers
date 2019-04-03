2378034942913fbea868cf3dfca87ac3cc7825dc
Tessa Pierce <ntpierce@gmail.com>
2019-04-03 15:04:43 -0700

    add trinity tests

--- elvers/tests/test_rules.py
+++ elvers/tests/test_rules.py
@@ -76,11 +76,20 @@ def test_salmon_short():
 def test_trimmomatic_short():
      run_ruletest('trimmomatic', 'test_files', {})
 
+def test_trimmomatic_long():
+     run_ruletest('trimmomatic', 'test_files', {}, short=False)
+
 def test_khmer_short():
      run_ruletest('khmer', 'test_files', {})
 
 def test_khmer_long():
      run_ruletest('khmer', 'test_files', {}, short=False)
+
+def test_trinity_short():
+     run_ruletest('trinity', 'test_files', {})
+
+def test_trinity_long():
+     run_ruletest('trinity', 'test_files', {}, short=False)
 
 
 #def test_salmon_long():
