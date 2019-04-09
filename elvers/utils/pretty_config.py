eec96db9d1b485c5e4483d79fa86c315069c1121
Tessa Pierce <ntpierce@gmail.com>
2019-04-09 12:11:22 -0700

    clean up

--- elvers/utils/pretty_config.py
+++ elvers/utils/pretty_config.py
@@ -31,7 +31,6 @@ def write_config(paramsD, targets, out = None):
     for targ in targets:
        # grab all rules, their params for target pipeliness
         include_rules = paramsD['elvers_workflows'].get(targ, [])
-        #targets = paramsD['elvers_workflows'][targ].get('targets', [])
         all_rules = include_rules+targets
         targ_params = {}
         for r in all_rules:
